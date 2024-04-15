cbdd94677bdba6fc52ce052ed60422e8392a2794
Faisal25DEC <faisalmd25121999@gmail.com>
2024-04-15 18:24:01 +0530

    scroll icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Home from "./pages/Home/Home";
 import Telephone from "./assets/telephone.png";
+import { useEffect, useState } from "react";
 function App() {
+  const [top, setTop] = useState(12);
+  useEffect(() => {
+    const handleScroll = () => {
+      const maxScrollableHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercent = (window.scrollY / maxScrollableHeight) * 100;
+      const newTop = Math.min(Math.max(scrollPercent, 12), 80); // Limiting the top value to a maximum of 50%
+      setTop(newTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <div className="">
-        <div className="right-0 top-[12%] translate-y-[-50%] fixed z-[99999999] flex flex-col gap-[18px]">
+        <div
+          style={{ top: `${top}%` }}
+          className="right-0 translate-y-[-50%] fixed z-[99999999] flex flex-col gap-[18px]"
+        >
           <button className="bg-white shadow-1 rounded-tl-[16px] shadow-2 rounded-bl-[16px] pl-[5px] ">
             <a
               href="https://wa.me/8904458815"
