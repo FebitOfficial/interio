38322426a6efe41d68bc315cb1bbc56b6f67b714
Faisal25DEC <faisalmd25121999@gmail.com>
2024-05-23 12:31:44 +0530

    emailjs

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { HomeContextProvider } from "./context/HomeContext/HomeContext";
+import { Toaster } from "sonner";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <HomeContextProvider>
+        <Toaster />
         <App />
       </HomeContextProvider>
     </BrowserRouter>
--- src/pages/Home/_components/Contact/Contact.jsx
+++ src/pages/Home/_components/Contact/Contact.jsx
@@ -1,9 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import Input from "../../../../components/Input/Input";
 import { HomeContext } from "../../../../context/HomeContext/HomeContext";
+import emailjs from "@emailjs/browser";
+import { toast } from "sonner";
 
 const Contact = ({ position, zIndex }) => {
   const { showContact } = useContext(HomeContext);
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uv1f95g",
+        "template_188rqvl",
+        formRef.current,
+        "AArrxq1S6Ul3yYZIk"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+          toast.success("Email sent successfully");
+        },
+        (error) => {
+          setError(true);
+          console.log(error);
+          toast.error("Something went wrong, please try again later");
+        }
+      );
+  };
   return (
     <div
       style={{ position, zIndex: zIndex || "-1" }}
@@ -14,29 +41,53 @@ const Contact = ({ position, zIndex }) => {
           Get in touch
         </p>
       </div>
-      <div className="flex flex-col lg:flex-row items-center lg:gap-[82px]">
-        <Input placeholder="Write Your Name" label="Name *" required />
-        <Input placeholder="Write Your Email" label="Email *" required />
-      </div>
-      <div className="flex items-end flex-col lg:flex-row  lg:gap-[82px]">
-        <Input placeholder="Your Budget" label="Budget" />
-        <div className="py-[12px] w-full flex flex-col gap-[6px] md:gap-[12px]">
-          <label className="text-black text-[16px] font-semibold">
-            About Your Project
-          </label>
-          <textarea
-            rows={5}
-            className="w-full border-[2px] p-[12px] text-black border-neutral-900 rounded-md"
-            placeholder="Project Info"
-            label="About Your Project"
+
+      <form className="flex flex-col" onSubmit={sendEmail} ref={formRef}>
+        <div className="flex flex-col lg:flex-row items-center lg:gap-[82px]">
+          <Input
+            placeholder="Write Your Name"
+            label="Name *"
+            required
+            name="name"
+          />
+          <Input
+            placeholder="Write Your Email"
+            label="Email *"
+            required
+            email="email"
           />
         </div>
-      </div>
-      <div className="w-full">
-        <button className="bg-black w-full rounded-md py-[16px] md:py-[26px] mt-[24px] hover:bg-bgPink transition-all ease-in duration-300 font-bold text-white text-[32px]">
-          Submit
-        </button>
-      </div>
+        <div className="flex items-end flex-col lg:flex-row  lg:gap-[82px]">
+          <Input
+            placeholder="Your Budget"
+            type="number"
+            label="Budget"
+            required
+            name="budget"
+          />
+          <div className="py-[12px] w-full flex flex-col gap-[6px] md:gap-[12px]">
+            <label className="text-black text-[16px] font-semibold">
+              About Your Project
+            </label>
+            <textarea
+              rows={5}
+              className="w-full border-[2px] p-[12px] text-black border-neutral-900 rounded-md"
+              placeholder="Project Info"
+              label="About Your Project"
+              required
+              name="message"
+            />
+          </div>
+        </div>
+        <div className="w-full">
+          <button
+            type="submit"
+            className="bg-black w-full rounded-md py-[16px] md:py-[26px] mt-[24px] hover:bg-bgPink transition-all ease-in duration-300 font-bold text-white text-[32px]"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
