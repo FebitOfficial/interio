a893626f3f615c85eb676f1817f54d82cc612a17
Faisal25DEC <faisalmd25121999@gmail.com>
2024-06-02 00:59:30 +0530

    icon changes

--- src/pages/Home/_components/Intro/index.js
+++ src/pages/Home/_components/Intro/index.js
@@ -1,22 +1,22 @@
 export const footerIcons = [
   {
     icon: "ri:facebook-line",
-    link: "https://www.google.com",
+    link: "https://www.facebook.com/MHinterioCom",
   },
   {
     icon: "ri:twitter-x-fill",
-    link: "https://www.google.com",
+    link: "https://x.com/MHinterioCom",
   },
   {
     icon: "mingcute:youtube-line",
-    link: "https://www.google.com",
+    link: "https://www.youtube.com/@MHinterioCom",
   },
   {
     icon: "prime:instagram",
-    link: "https://www.google.com",
+    link: "https://www.instagram.com/mhinteriocom/",
   },
   {
     icon: "fa6-brands:pinterest-p",
-    link: "https://www.google.com",
+    link: "https://www.pinterest.com/mhinteriocom/",
   },
 ];
